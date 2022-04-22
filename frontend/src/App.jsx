2a09af690dc6d00d396c342a025ab01790f8cc51
TreezCode <TreezCode@gmail.com>
2022-04-22 02:30:35 -0400

    added frontend UI base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import * as React from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import DarkMode from './components/DarkMode';
 
 function App() {
   return (
-    <div className="App">
-        <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <DarkMode />
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ frontend/src/components/DarkMode.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import '../index.css';
+
+const setDark = () => {
+  localStorage.setItem('theme', 'dark');
+  document.documentElement.setAttribute('data-theme', 'dark');
+};
+const setLight = () => {
+  localStorage.setItem('theme', 'light');
+  document.documentElement.setAttribute('data-theme', 'light');
+};
+
+// Check the users theme preference
+const storedTheme = localStorage.getItem('theme');
+const prefersDark =
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches;
+const defaultDark =
+  storedTheme === 'dark' || (storedTheme === null && prefersDark);
+if (defaultDark) {
+  setDark();
+}
+
+const toggleTheme = (e) => {
+  if (e.target.checked) {
+    setDark();
+  } else {
+    setLight();
+  }
+};
+
+function DarkMode() {
+  return (
+    <>
+      <span className="dark-mode">
+        <span>☀️</span>
+        <input
+          type="checkbox"
+          name="switch"
+          id="switch"
+          onChange={toggleTheme}
+          defaultChecked={defaultDark}
+        />
+        <label htmlFor="switch"></label>
+        <span>🌘</span>
+      </span>
+    </>
+  );
+}
+
+export default DarkMode;
